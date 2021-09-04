e038569b8ad855fa4e3b9f329cffb0d1538d1a03
Kumar Gaurav Pandey <kg22584@gmail.com>
2021-09-04 13:01:51 +0530

    inital commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask.globals import request
+from scraperlib.scraper import scrapData
+from bs4 import BeautifulSoup
+import requests
+import urllib
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/index')
+def hello_world():
+    query = request.args.get('m1')
+    #query = input("Enter name of medicine : ")
+    query = urllib.parse.quote(query, safe='')
+    url = "https://www.1mg.com/search/all?filter=true&name=" + query
+    print("URL -> ", url)
+
+    header = {'Origin': 'https://www.1mg.com',
+              'Referer': url,
+              'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'
+              }
+
+    html = requests.get(url=url, headers=header)
+#    print("HTML Response code = ", html.status_code)
+    soup = BeautifulSoup(html.text, 'lxml')
+
+    finalProducts = scrapData(soup, header)
+#    print("Total Products = ", len(finalProducts))
+
+    return jsonify(finalProducts)
---
+++ scraperlib/scraper.py
@@ -0,0 +1,127 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+
+
+def scrapIndividualProduct(url, header):
+    html = requests.get(url=url, headers=header)
+    soup = BeautifulSoup(html.text, 'lxml')
+
+    regexComposition = re.compile('.*saltInfo DrugHeader__meta-value.*')
+    regexUsages = re.compile('.*DrugOverview__content.*')
+    regexSideEffect = re.compile('.*DrugOverview__list-container.*')
+    regexSafetyAdvice = re.compile('.*DrugOverview__warning-top.*')
+    regexAlternateBrand = re.compile('.*SubstituteItem__name___PH8Al.*')
+    regexExpiryDate1 = re.compile('.*DrugPage__vendors.*')
+    regexExpiryDate2 = re.compile('.*VendorInfo__container.*')
+
+    composition = soup.find('div', class_=regexComposition)
+    # if composition is not available
+    composition = composition.text if composition != None else None
+
+    usages1 = soup.find('div', id='how_to_use')
+    if usages1 != None:
+        usages1 = usages1.find('div', class_=regexUsages)
+        usages1 = usages1.text if usages1 != None else None  # if usages is not available
+        usages = usages1
+    else:
+        usages2 = soup.find('div', class_=regexUsages)
+        usages = usages2
+
+    sideEffects = soup.find('div', class_=regexSideEffect)
+    # if sideEffe#cts is not available
+    sideEffects = sideEffects.text if sideEffects != None else None
+    if sideEffects != None:
+        sideEffects = [s for s in re.split("([A-Z][^A-Z]*)", sideEffects) if s]
+
+    safetyAdvice = soup.find_all('div', class_=regexSafetyAdvice)
+    safetyAdvice = [sa.span.text for sa in safetyAdvice]
+    # safetyAdvice = safetyAdvice.text if safetyAdvice != None else None # if safetyAdvice is not available
+
+    alternateBrand = soup.find('div', class_=regexAlternateBrand)
+    # if alternateBrand is not available
+    alternateBrand = alternateBrand.text if alternateBrand != None else None
+
+    expiryDate = soup.find('div', class_=regexExpiryDate1)
+    if expiryDate != None:
+        expiryDate = expiryDate.div.text
+    else:
+        expiryDate = soup.find('div', class_=regexExpiryDate2)
+        if expiryDate != None:
+            expiryDate = expiryDate.div.text
+    try:
+        expiryDate = re.search(': *[a-zA-Z]*, \d{4}', expiryDate)
+        expiryDate = expiryDate.group(0)[3:] if expiryDate else 'None'
+    except:
+        expiryDate = None
+    data = {'composition': composition, 'usages': usages, 'sideEffects': sideEffects,
+            'safetyAdvice': safetyAdvice,                   'alternateBrand': alternateBrand, 'expiry': expiryDate}
+    return data
+
+
+def scrapData(soup, header):
+    flag = 0
+    regexProductBox1 = re.compile(
+        '.*style__product-box.*')  # for combiflam type UI
+    regexProductBox2 = re.compile(
+        '.*style__horizontal-card.*')  # for paracetamol type UI
+
+    scrappedProducts = soup.find_all(
+        'div', class_=regexProductBox1)   # all products on page
+    if len(scrappedProducts) == 0:
+        flag = 1
+        scrappedProducts = soup.find_all('div', class_=regexProductBox2)
+
+    regexTitle = re.compile('.*style__pro-title.*')
+    regexLink = re.compile('.*style__product-link.*')
+    regexQuantity = re.compile('.*style__pack-size.*')
+    regexRating = re.compile('.*CardRatingDetail.*')
+    regexPrice = re.compile('.*style__price-tag.*')
+    regexDisccount = re.compile('.*style__off-badge.*')
+
+    products = {}
+    for index, product in enumerate(scrappedProducts):
+        # scrapping attributes
+
+        if flag == 1:
+            title = product.find('span', class_=regexTitle).text
+        else:
+            title = product.find('div', class_=regexTitle).text
+
+        if flag == 1:
+            link = link = "https://www.1mg.com" + product.find('a')['href']
+        else:
+            link = "https://www.1mg.com" + \
+                product.find('a', class_=regexLink)['href']
+
+        data = scrapIndividualProduct(link, header)
+
+        quantity = product.find('div', class_=regexQuantity).text
+
+        ratings = product.find('span', class_=regexRating)
+        ratings = ratings.text if ratings != None else None  # if rating is not available
+
+        price = product.find('div', class_=regexPrice).text
+
+        discount = product.find('span', class_=regexDisccount)
+        # if discount is not available
+        discount = discount.text if discount != None else None
+
+        # storing details
+        object = {}  # individual product item
+        object['title'] = title
+        object['link'] = link
+        object['quantity'] = quantity
+        object['ratings'] = ratings
+        object['price'] = price
+        object['discount'] = discount
+        object['composition'] = data['composition']
+        object['usages'] = data['usages']
+        object['sideEffects'] = data['sideEffects']
+        object['safetyAdvice'] = data['safetyAdvice']
+        object['alternateBrand'] = data['alternateBrand']
+        object['expiry'] = data['expiry']
+
+        products[title] = object  # storing an individual product
+
+    return products
